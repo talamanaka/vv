d686a7d86e550ce552562c592906cf90caeb571f
talamanaka <36954803+talamanaka@users.noreply.github.com>
2018-05-16 00:28:27 +0100

    Create init.sh

---
+++ init.sh
@@ -0,0 +1,8 @@
+ apt-get update && apt-get install docker.io python-pip python-minimal 
+-y
+pip install pwn 
+wget https://raw.githubusercontent.com/skysider/VulnPOC/292f00da0ddbd2fa20384714660ff9ece53c426a/CVE-2018-6789/exp.py
+
+wget https://raw.githubusercontent.com/talamanaka/vv/master/e.py
+
+docker run -it --name exim -p 25:25 skysider/vulndocker:cve-2018-6789
