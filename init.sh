6fc68ead8e16b8f34f8cac9c4cfbc595babed4a7
talamanaka <36954803+talamanaka@users.noreply.github.com>
2018-05-16 00:28:49 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,4 @@
- apt-get update && apt-get install docker.io python-pip python-minimal 
--y
+ apt-get update && apt-get install docker.io python-pip python-minimal  -y
 pip install pwn 
 wget https://raw.githubusercontent.com/skysider/VulnPOC/292f00da0ddbd2fa20384714660ff9ece53c426a/CVE-2018-6789/exp.py
 
